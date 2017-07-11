3da3519edb639f085a616e06504661ce65728d6e
Mark <Mark@DESKTOP-CT2BJP6.wireless.txstate.edu>
2017-07-11 18:17:25 -0500

    Initial commit

---
+++ src/BHFermentation/data/Chamber/Heater.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BHFermentation.data.Chamber;
+
+import BHFermentation.data.Component;
+
+/**
+ *
+ * @author Mark
+ */
+public class Heater extends Component {
+    
+}
