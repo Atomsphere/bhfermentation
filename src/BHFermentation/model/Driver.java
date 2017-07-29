342d791126f168a7e51235e45b1e6ae3d8f812c6
Mark Maupin <m.w.maupin@gmail.com>
2017-07-29 15:03:41 -0500

    changed how process controller is going to call pid and created a driver to better match the architecture. also changed the colors so that text was visible (not an implication of artistic direction)

---
+++ src/BHFermentation/model/Driver.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BHFermentation.model;
+
+import BHFermentation.view.JFrameView;
+
+/**
+ *
+ * @author admin
+ */
+public class Driver {
+    
+     public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFrameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFrameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFrameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFrameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFrameView().setVisible(true);
+        });
+    }
+}
--- src/BHFermentation/model/ProcessController.java
+++ src/BHFermentation/model/ProcessController.java
@@ -7,18 +7,14 @@ package BHFermentation.model;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 
 /**
  *
  * @author Mark
  */
-public class ProcessController extends Application{
+public class ProcessController {
 
     
     final static GpioController gpio = GpioFactory.getInstance();
@@ -30,20 +26,7 @@ public class ProcessController extends Application{
      * @param stage
      * @throws Exception 
      */
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLView.fxml"));
         
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
-    
-     public static void main(String[] args) {
-        launch(args);
-    }
-    
     /**
      *  default constructor
      */
@@ -54,29 +37,4 @@ public class ProcessController extends Application{
     
     
     
-    /**
-     * to be filled out later
-     */
-    public class PID{
-        private float kph, kdh, kih;
-        private float kpc, kdc, kic;
-        
-        
-        
-        public void cooling(int setpoint, int interval, String keyword){
-            
-        }
-        
-        public void heating(int setpoint, int interval, String keyword){
-            
-        }
-        
-        public void heatingTune(){
-            
-        }
-        
-        public void coolingTune(){
-            
-        }
-}
 }
