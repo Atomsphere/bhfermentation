662bec02893d2998dcb848e70ce5e078f7fe929c
blm153 <blm153@txstate.edu>
2017-08-02 15:37:08 -0500

    Setting up Test Driver for future JUnit tests

---
+++ src/BHFermentation/model/TestDriver.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BHFermentation.model;
+
+import org.junit.*;
+
+/**
+ *
+ * @author Lane M
+ */
+public class TestDriver {
+    
+    @Test
+    public void processControllerTest()
+    {
+        ProcessController testPC = new ProcessController();
+        
+        // Test cases here
+    }
+    
+    @Test
+    public void PIDUnitTest()
+    {
+        PIDunit testPID = new PIDunit();
+        
+        // Test cases here
+    }
+}
