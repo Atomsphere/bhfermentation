f30ea4a28b7b1e096cdeb80310f814293a4d1f17
blm153 <blm153@txstate.edu>
2017-08-08 21:49:36 -0500

    test

--- test/BHFermentationTests/TestDriver.java
+++ test/BHFermentationTests/TestDriver.java
@@ -20,10 +20,13 @@ import static org.junit.Assert.*;
  */
 public class TestDriver {
     
+    
+    ProcessController testPC = new ProcessController();
+    
     @Test
     public void heater1Test()
     {
-        ProcessController testPC = new ProcessController();
+        
         testPC.setHeater1(true);
         assertEquals("Result: false. Heater 1 should be set to true.", "On", testPC.getHeater1State());
     }
@@ -31,7 +34,6 @@ public class TestDriver {
     @Test
     public void heater2Test()
     {
-        ProcessController testPC = new ProcessController();
         testPC.setHeater2(true);
         assertEquals("Result: false. Heater 2 should be set to true.", "On", testPC.getHeater2State());
     }
@@ -39,7 +41,6 @@ public class TestDriver {
     @Test
     public void fan1Test()
     {
-        ProcessController testPC = new ProcessController();
         testPC.setFan1(true);
         assertEquals("Result: false. Fan 1 should be set to true.", "On", testPC.getFan1State());
     } 
@@ -47,7 +48,6 @@ public class TestDriver {
     @Test
     public void fan2Test()
     {
-        ProcessController testPC = new ProcessController();
         testPC.setFan2(true);
         assertEquals("Result: false. Fan 2 should be set to true.", "On", testPC.getFan2State());
     } 
